bc9474484ff4e10d07d8739a13fe9515e3f92a14
Orlando Groppo <orlando@Orlandos-MacBook-Pro.local>
2017-10-19 18:44:48 +0100

    Auto commit

--- node_modules/fluent-cypher/tests/merge.test.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import CypherQuery from '../index';
-
-test('MERGE clause', t => {
-	t.is(new CypherQuery().merge().queryString, 'MERGE ')
-	t.is(new CypherQuery().merge('custom string').queryString, 'MERGE custom string ')
-	t.is(new CypherQuery().merge('pattern1', 'pattern2').queryString, 'MERGE pattern1, pattern2 ')
-});
--- node_modules/isitnot/lib/boolean.js
+++
@@ -1,3 +0,0 @@
-isBoolean = arg => Object.prototype.toString.call(arg) === '[object Boolean]'
-
-isNotBoolean = arg => !isBoolean(arg)
